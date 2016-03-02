dac9e1eb2766e2f23d11ea43eab1e49668d6ede7
CKiilu <cndeti@gmail.com>
2016-03-02 19:56:00 +0300

    Added layouts

---
+++ app/src/main/java/com/scurrae/chris/lyricssmash/Songs1.java
@@ -0,0 +1,13 @@
+package com.scurrae.chris.lyricssmash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Songs1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.octo);
+    }
+}
---
+++ app/src/main/java/com/scurrae/chris/lyricssmash/Songs3.java
@@ -0,0 +1,13 @@
+package com.scurrae.chris.lyricssmash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Songs3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esty);
+    }
+}
